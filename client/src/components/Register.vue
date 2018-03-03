ba70e76f940c84c76b9e2b7b857b6d29eab7bac4
monikakalt <kaltenytmonika3@gmail.com>
2018-03-04 00:10:39 +0200

    register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email" name="email" v-model="email" placeholder="Email"/>
+    <input type="password" name="password" v-model="password" placeholder="Password"/>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
