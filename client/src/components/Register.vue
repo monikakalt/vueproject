5c25da28a8168d5e2dd01a85b6e24b4ac26ded28
monikakalt <kaltenytmonika3@gmail.com>
2018-03-05 22:59:38 +0200

    vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="cyan" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
 
-    <input type="email" name="email" v-model="email" placeholder="Email"/>
-    <input type="password" name="password" v-model="password" placeholder="Password"/>
-    <button @click="register">Register</button>
-  </div>
+            <div class="pl-4 pr-4 pt-2 pb-2">
+              <input type="email" name="email" v-model="email" placeholder="Email"/>
+              <input type="password" name="password" v-model="password" placeholder="Password"/>
+              <br>
+              <div class="error" v-html="error"></div>
+              <br>
+              <v-btn class="cyan" @click="register">Register</v-btn>
+            </div>
+        </div>
+    </v-flex>
+  </v-layout>
+
 </template>
 
 <script>
@@ -14,7 +26,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   watch: {
@@ -24,12 +37,23 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try{
+          await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      }catch (error) {
+          this.error = error.response.data.error
+      }
+
     }
   }
 }
 </script>
+
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
